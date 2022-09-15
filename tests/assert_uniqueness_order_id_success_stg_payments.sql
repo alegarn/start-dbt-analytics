023d62a3e7fc9b249e7e023a4c339ea7fe0a963d
alexgarnier78310@protonmail.com <alexgarnier78310@protonmail.com>
2022-09-15 21:10:38 +0000

    documentation

--- tests/assert_uniqueness_order_id_success_stg_payments.sql
+++ tests/assert_uniqueness_order_id_success_stg_payments.sql
@@ -2,13 +2,24 @@ with payments as (
     select *
     from {{ ref('stg_payments')}}
     where
-        STATUS = 'success'
+        STATUS = 'success' AND
+        amount > 0
+),
+
+status_success as (
+    select 
+        --payment_id,
+        order_id,
+        count(distinct order_id),
+        count(order_id)
+    from
+        payments
+    group by
+        order_id
 )
 
 select 
-    order_id,
-    CASE WHEN count(distinct order_id)= count(order_id) then 'order_id uniq' ELSE 'column values are NOT unique' end as order_uniq
+    *
+    --CASE WHEN count(distinct order_id)= count(order_id) then 'order_id uniq' ELSE 'column values are NOT unique' end as order_uniq
 from
-    payments
-group by
-    order_id
+    status_success
