ff9ca500636b9143afd1c72a23c4e15825e8b15f
alexgarnier78310@protonmail.com <alexgarnier78310@protonmail.com>
2022-09-28 19:21:53 +0000

    refactor final model fct_customer_orders.sql

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -30,31 +30,24 @@ customer_orders as (
     
 ),
 
--- F.n.l CTE
+-- Final CTE
 final as (
 
     select
 
         p_o.*,
--- ch.que ord.r m..s n.n f..l
-        ROW_NUMBER() OVER (
-            ORDER BY p_o.order_id
-            ) as transaction_seq,
 
--- c.st: Xe f..s c.mm.nde
         ROW_NUMBER() OVER (
             PARTITION BY customer_id 
             ORDER BY p_o.order_id
             ) as customer_sales_seq,
 
--- t.ble is_n.w?
         CASE 
             WHEN c.first_order_date = p_o.order_placed_at
             THEN 'new'
             ELSE 'return' END 
-            as new_order?,
+            as new_or_return_order,
 
-        cus_tot_paid.cust_lifetime_val as customer_lifetime_value,
         c.first_order_date as cst_frst_ord_dt
 
     FROM 
@@ -62,9 +55,6 @@ final as (
 
     left join
         customer_orders as c USING (customer_id)
-
-    LEFT OUTER JOIN total_amount_paid_per_cust as cus_tot_paid
-        on cus_tot_paid.order_id = p_o.order_id
         
     ORDER BY order_id
 
